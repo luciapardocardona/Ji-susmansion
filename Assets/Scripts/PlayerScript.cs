2fbb92e8c8c935f827d10915037a63c00ceb4021
KuroNeko-98 <110256022+KuroNeko-98@users.noreply.github.com>
2022-07-30 17:33:50 +0200

    Player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-    public int jump = 10, vel = 8; 
-    Rigidbody2D rb;
+    public int jump = 10, vel = 8;
+    Rigidbody2D myRigidbody;
     float enX;
-    public SpriteRenderer sprite; //Vamos a hacer un flip al player
-    //bool bloqueo;//, girado;
+    public SpriteRenderer sprite;
     public Animator animacion;
     public AudioClip soundsalto;
     AudioSource sonido;
@@ -24,84 +23,10 @@ public class PlayerScript : MonoBehaviour
         movement = GetComponent<PlayerMovement>();
         sonido = GetComponent<AudioSource>();
         capsule = GetComponent<CapsuleCollider2D>();
-        rb = GetComponent<Rigidbody2D>();
-
-        //bloqueo = false;
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
-    void Update() {
+    void Update()
+    {
         movement.Run();
-        /*
-        if (!bloqueo){
-            Jump();
-        }
-
-        if (rb.velocity.x > 0){
-            sprite.flipX = false;
-            //girado = false; // para poder lanzar objetos
-            capsule.offset = new Vector2(-0.27f, 0);
-        }else if (rb.velocity.x < 0){
-            sprite.flipX = true;
-            //girado = true; // para poder lanzar objetos
-            capsule.offset = new Vector2(0.27f, 0);
-        }
-        
-        AnimarPlayer();
-        */
     }
-/*
-
-    private void AnimarPlayer(){
-        if (bloqueo){
-            animacion.Play(AnimationConstants.Idle);
-        }else{
-            if (Input.GetKey(KeyCode.E)){
-                animacion.Play(AnimationConstants.MoradoPrender);
-            }else if (((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.D))) && (Ground())){
-                animacion.Play("Walk");
-            }else if (!Ground()){
-                animacion.Play("Jump");
-            }else{ 
-                animacion.Play("Idle");
-            }
-        }
-    }
-
-    void FixedUpdate(){
-        if (!bloqueo){
-            enX = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(enX * vel, rb.velocity.y);
-        }
-    }
-
-    private void Jump(){
-        if (((Input.GetKeyDown(KeyCode.W))) && Ground()){
-            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-            sonido.PlayOneShot(soundsalto);
-        }
-    }
-
-    private bool Ground(){
-        RaycastHit2D toca = Physics2D.Raycast(transform.position, Vector2.down, 1.45f);
-        if (toca.collider == null){ // Si no toca ninguna
-            return false;
-        }else{ // Si toca alguna
-            return true;
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D other) {
-        //if (other.gameObject.CompareTag(TagConstants.Interruptor)){
-        //    if (Input.GetKeyDown(KeyCode.E)){
-        //        other.SendMessage("Activar");
-        //    }
-        //}
-        //if (other.gameObject.CompareTag(TagConstants.Door))
-        //{
-        //    if(Input.GetKeyDown(KeyCode.E)){
-        //        Invoke(nameof(ANivel2), 0.1f);
-        //    }
-        //}
-    }
-*/
-   
 }
