044a55b786824bc5bf0791f0aecd7e4f4b2a2b1b
Baluj96 <Baluj55555@gmail.com>
2022-07-30 10:14:17 +0200

    Menu y Creditos

--- Assets/Scripts/CreditsScript.cs
+++ Assets/Scripts/CreditsScript.cs
@@ -5,7 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class CreditsScript : MonoBehaviour
 {
-    void Update(){
+    public GameObject press;
+
+    private void Start(){
+        press.SetActive(false);
+        Invoke("Continua", 15f);
+    }
+
+    private void Continua(){
+        press.SetActive(true);
         if (Input.anyKey){
             SceneManager.LoadScene("Menu");
         }
