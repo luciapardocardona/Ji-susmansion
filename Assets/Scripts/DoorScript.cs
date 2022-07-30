8724fa772437f1afe380df0887bfef033c7c1005
Ignacio Pardo Cardona <ignacio@doggiesintown.com>
2022-07-30 18:19:03 +0200

    Removed unused Logic on doorscript

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-
-    [SerializeField]
-    Camera mainCamera;
-
     [SerializeField]
     public bool isCorrectDoor = false;
-
-    GameManager GameManager;
-
-    private void Awake()
-    { 
-        this.GameManager = mainCamera.GetComponent<GameManager>();
-    }
 }
