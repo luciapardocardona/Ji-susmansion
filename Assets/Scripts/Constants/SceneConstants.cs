1056864370950bfc45f5e2824230ec6004f69260
Baluj96 <Baluj55555@gmail.com>
2022-07-31 13:55:02 +0200

    finales

--- Assets/Scripts/Constants/SceneConstants.cs
+++ Assets/Scripts/Constants/SceneConstants.cs
@@ -9,4 +9,6 @@ public class SceneConstants
     public const string Nivel3 = "Nivel3";
     public const string Creditos = "Creditos";
     public const string Menu = "Menu";
+    public const string FinalBueno = "Final Bueno";
+    public const string FinalMalo = "Final Malo";
 }
