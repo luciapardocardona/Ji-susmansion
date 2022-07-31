80315c01a69c41caa7ec197d4efca7d128d002a4
KuroNeko-98 <110256022+KuroNeko-98@users.noreply.github.com>
2022-07-31 12:12:43 +0200

    Audio y random npc

--- Assets/Scripts/NPCsMovement.cs
+++ Assets/Scripts/NPCsMovement.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class NPCsMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] List<GameObject> monsters;
+    bool isLightOn = false;
+    List<float> positions = new List<float> { -1f, -4f, -7f };
+    float posY = 3f;
+    float posZ = 0f;
 
-    void Update()
+    void MoveMonsters()
     {
-        
+        List<int> tmpMonsters = new List<int> { };
+
+        foreach (var position in positions)
+        {
+            int index = Random.Range(0, monsters.Count);
+            tmpMonsters.Add(index);
+            var monster = monsters[index];
+            monster.transform.Translate(position,posY,posZ);
+            //monster.GetComponent<NPCsDialogSystem>
+        }
     }
 }
